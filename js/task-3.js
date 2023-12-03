e7d800b20ae0d8779a3ada136d47a58438d8c594
OlikKorsun <145294542+OlikKorsun@users.noreply.github.com>
2023-12-03 19:27:41 +0200

    task1+task2+task3+all_include_in_html

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+let content = "120px";
+let padding = "150px";
+let border = "1.2px";
+
+function getElementWidth(content, padding, border) {
+    return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
